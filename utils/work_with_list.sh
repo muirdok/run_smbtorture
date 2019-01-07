217f7e149228d008dab32411918e821c50f2f257
Georgy <muirdok@gmail.com>
2019-01-07 17:17:06 +0000

    cleaning

--- utils/work_with_list.sh
+++ utils/work_with_list.sh
@@ -5,7 +5,6 @@ do
     do
 #      python3 create_case_rail.py -t $word
       echo "RUN CASE: $word"
-#     ~/samba/bin/smbtorture -t 600 -U admin%Nexenta1! '//192.168.79.227/kek_fs'  $word
 	python3.5 create_case_rail.py -t $word
 
     done
